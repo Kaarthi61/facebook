20e25b14b4a8a2fdc80833a01aeb238fc85f93f8
kaarthik61 <kdkarthik16@gmail.com>
2022-02-19 18:55:04 +0530

    t

---
+++ src/com/individuallogin/CreatNewAccount.java
@@ -0,0 +1,24 @@
+package com.individuallogin;
+
+public class CreatNewAccount {
+	
+	private void name() {
+		System.out.println("suganesh");
+		
+	}
+	private void emailId() {
+		System.out.println("varatharajansuganesh@.com");
+
+	}
+	private void password() {
+		System.out.println("7678");
+
+	}
+	public static void main(String[] args) {
+		CreatNewAccount b=new CreatNewAccount();
+		b.name();
+		b.emailId();
+		b.password();
+	}
+
+}
